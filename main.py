948d30b5ab0792e6b869a22741684ad458cdcf18
Rolfc8 <134684408+Rolfc8@users.noreply.github.com>
2023-05-26 10:43:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def index():
     return"hola a todos, ¿quieres saber sobre recetas para cocinar?"
 @app.get("/recetas/(num)")
 def recetas(num):
-    recetas={
+    receta={
         "1":"receta para pan de muerto",
         "2":"receta para pan de elote",
         "3":"receta para pan de zanahoria",
         "4":"receta para empanadas"
            }
-    return (recetas[num])
+    return (receta[num])
   
